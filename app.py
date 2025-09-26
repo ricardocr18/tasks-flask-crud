f9fa9e5d231782cf363dcab2bf46dc3643f8616d
ricardocrNatura <ricardoribeiro.compasso@natura.net>
2025-09-26 12:35:25 -0300

    Implement task management functionality with CRUD operations

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 # __name__ = "__main__"
 app = Flask(__name__)
 
-@app.route("/")
-def hello_word():
-    return "Hello, World!"
 
-@app.route("/about")
-def about():
-    return "This is the about page."    
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description', ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print (tasks)
+    return jsonify({"message": "Task created successfully"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": len(task_list)
+            }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possível encontrar a tarefa"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
